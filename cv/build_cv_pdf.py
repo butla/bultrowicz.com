cc433a352504bc2f47f7478f46511c1648d4f2c3
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2023-10-02 22:42:10 +0200

    CV and CV generation update.

--- cv/build_cv_pdf.py
+++ cv/build_cv_pdf.py
@@ -4,16 +4,16 @@ from playwright.sync_api import sync_playwright
 
 
 def render_cv_pdf():
-    cv_source_file = (Path(__file__) / '../cv_Michał_Bultrowicz.html').resolve().as_uri()
+    cv_source_file = (Path(__file__) / '../cv_Michał_Bultrowicz.html').resolve()
 
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(executable_path='/usr/bin/chromium')
         page = browser.new_page()
-        page.goto(cv_source_file)
+        page.goto(cv_source_file.as_uri())
         # Looks like `await page.emulate_media(media='screen')` from the docs
         # (https://playwright.dev/python/docs/api/class-page#page-pdf) doesn't change anything.
         page.pdf(
-            path='cv_Michał_Bultrowicz.pdf',
+            path=(cv_source_file / '../cv_Michał_Bultrowicz.pdf').resolve(),
             format='A4',
             # Looks like you can do the below or add `-webkit-print-color-adjust: exact;` in CSS to preserve
             # background colors.
